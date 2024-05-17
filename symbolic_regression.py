1d65cf8fed16fb215cf7f4d1ed592397f9d8d4df
Raul Perbichi <rperbichi97@gmail.com>
2024-05-16 21:04:02 -0600

    Still need to fix rule notation

--- symbolic_regression.py
+++ symbolic_regression.py
@@ -47,4 +47,8 @@ plt.show()
 print("GE Section:")
 rules = {
     # TODO: create rules
+    "<expr> ::= <expr> <op> <expr> | <var> | <const>",
+    "<op>   ::= + | - | * | /",
+    "<var>  ::= x | y | z",
+    "<const> ::= 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9"
 }
