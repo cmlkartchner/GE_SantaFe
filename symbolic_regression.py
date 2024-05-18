f2c3a9adfde1d34f9bfa0d0fab8d80948914245e
Raul Perbichi <rperbichi97@gmail.com>
2024-05-17 18:07:31 -0600

    Symbolic regression works

--- symbolic_regression.py
+++ symbolic_regression.py
@@ -1,113 +1,104 @@
-import numpy as np
+import operator
 import random
+import numpy as np
 import matplotlib.pyplot as plt
+from functools import partial
 from sympy import symbols, Eq, solve, init_printing
-from deap import base, creator, tools, gp, generate
+from deap import base, creator, tools, gp, algorithms
 
-init_printing() 
+def safe_div(num, den):
+    if den == 0:
+        return 1
+    else:
+        return num / den
 
-random.seed(45) # we set the seed to the same number so we don't need to set it each time
+def rand101():
+    return random.randint(-10, 10)
 
-x = np.linspace(-10, 10, 200) # input data
+init_printing()
 
+random.seed(45)
+
+x = np.linspace(-10, 10, 200)
 true_relationship = lambda x: x**2 + 2*x + 1
-
-'''
-Now we need to generate the output
-We will use noise values since we are working with synthetic data
-Noise values basically means the variation use to simulate real world imperfections
-'''
-
-noise = np.random.normal(0, 120, x.shape) 
 '''
     - 0 is the mean 
     - 10 is the standard deviation 
     - x.shape takes in account all input values for the size of the array
     - try: change standard deviation to test data plot
 '''
-print("Number of input values:", str(x.size))
+noise = np.random.normal(0, 120, x.shape)
 y = true_relationship(x) + noise
 
-# Symbolic expression: y = x^2 + 2x + 1
+print("Number of input values:", x.size)
 x_sym = symbols('x')
-expression_sym = x_sym**2 + 2*x_sym + 1
-print("Expression: y = " + str(expression_sym))
+expression_sym = x_sym**2 + 2*x_sym + 1 # symbolic expression
+print("Expression: y =", expression_sym)
 
-x_sym = symbols('x')
-solutions = solve(Eq(expression_sym,0), x_sym)
-print("Solutions for Y:", solutions)
+solutions = solve(Eq(expression_sym, 0), x_sym)
+print("Solutions for y = 0:", solutions)
 
-# Plotting the data
-plt.scatter(x, y, label = "noise")
-plt.plot(x, true_relationship(x), color = 'red', label = 'True Rleationship')
-plt.legend()
-plt.show()
+pset = gp.PrimitiveSet("MAIN", 1)
+pset.addPrimitive(operator.add, 2)
+pset.addPrimitive(operator.sub, 2)
+pset.addPrimitive(operator.mul, 2)
+pset.addPrimitive(safe_div, 2)
+pset.addEphemeralConstant("rand101", partial(rand101))
+pset.renameArguments(ARG0 = 'x')
 
-# TODO: implement GE to find mathematical models that fit the noise data
-print("GE Section:")
-import numpy as np
-import random
-import matplotlib.pyplot as plt
-from sympy import symbols, Eq, solve, init_printing
-from deap import base, creator, tools, gp
-
-init_printing() 
-
-random.seed(45) # we set the seed to the same number so we don't need to set it each time
-
-x = np.linspace(-10, 10, 200) # input data
-
-true_relationship = lambda x: x**2 + 2*x + 1
-
-'''
-Now we need to generate the output
-We will use noise values since we are working with synthetic data
-Noise values basically means the variation use to simulate real world imperfections
-'''
-
-noise = np.random.normal(0, 120, x.shape) 
-'''
-    - 0 is the mean 
-    - 10 is the standard deviation 
-    - x.shape takes in account all input values for the size of the array
-    - try: change standard deviation to test data plot
-'''
-print("Number of input values:", str(x.size))
-y = true_relationship(x) + noise
-
-# Symbolic expression: y = x^2 + 2x + 1
-x_sym = symbols('x')
-expression_sym = x_sym**2 + 2*x_sym + 1
-print("Expression: y = " + str(expression_sym))
-
-x_sym = symbols('x')
-solutions = solve(Eq(expression_sym,0), x_sym)
-print("Solutions for Y:", solutions)
-
-# Plotting the data
-plt.scatter(x, y, label = "noise")
-plt.plot(x, true_relationship(x), color = 'red', label = 'True Rleationship')
-plt.legend()
-plt.show()
-
-# TODO: implement GE to find mathematical models that fit the noise data
-print("GE Section:")
-
-rules = {
-    "<expr>": [["<expr>", "<op>", "<expr>"], ["<var>"], ["<const>"]],
-    "<op>": [["+"], ["-"], ["*"], ["/"]],
-    "<var>": [["x"]],
-    "<const>": [["0"], ["1"], ["2"], ["3"], ["4"], ["5"], ["6"], ["7"], ["8"], ["9"]]
-}
-
-'''
-    - Fitness: how well an individual meets the qualifications to solve the problem
-'''
-creator.create("Minimum fitness: ", base.Fitness, weights = (-1.0,))
-creator.create("Maximum fitness: ", base.Fitness, weights = (1.0,))
+creator.create("FitnessMin", base.Fitness, weights = (-1.0,))
+creator.create("Individual", gp.PrimitiveTree, fitness = creator.FitnessMin)
 
 toolbox = base.Toolbox()
-toolbox.register("expr", generate, rules = rules, max_depth = 10)
+toolbox.register("expr", gp.genHalfAndHalf, pset = pset, min_= 1, max_ = 3)
+toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.expr)
+toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+toolbox.register("compile", gp.compile, pset = pset)
 
-def eval_genetic_expression():
-    
+def eval_genetic_expression(individual):
+    func = toolbox.compile(expr = individual)
+    try:
+        predictions = np.array([func(xi) for xi in x])
+        mse = ((y - predictions) ** 2).mean()
+    except Exception as e:
+        mse = np.inf
+    return (mse,)
+
+toolbox.register("evaluate", eval_genetic_expression)
+toolbox.register("select", tools.selTournament, tournsize = 3)
+toolbox.register("mate", gp.cxOnePoint)
+toolbox.register("mutate", gp.mutUniform, expr = toolbox.expr, pset = pset)
+toolbox.decorate("mate", gp.staticLimit(key = operator.attrgetter("height"), max_value = 17))
+toolbox.decorate("mutate", gp.staticLimit(key = operator.attrgetter("height"), max_value = 17)) 
+
+def main():
+    pop = toolbox.population(n = 300)
+    hof = tools.HallOfFame(1)
+
+    stats = tools.Statistics(lambda ind: ind.fitness.values)
+    stats.register("avg", np.mean)
+    stats.register("min", np.min)
+    stats.register("max", np.max)
+
+    algorithms.eaSimple(pop, toolbox, 0.5, 0.2, 40, stats = stats, halloffame = hof, verbose = True)
+
+    return pop, stats, hof
+
+if __name__ == "__main__":
+    final_pop, stats, hof = main()
+    best_func = toolbox.compile(expr = hof[0])
+    predictions = [best_func(xi) for xi in x]
+
+    '''
+        Now we need to generate the output
+        We will use noise values since we are working with synthetic data
+        Noise values basically means the variation use to simulate real world imperfections
+    '''
+
+    plt.scatter(x, y, color = 'red', label = 'Noisy Data')
+    plt.plot(x, true_relationship(x), label = 'True Relationship')
+    plt.plot(x, predictions, label = 'Evolved Model')
+    plt.title("Comparison of True Relationship and Evolved Model")
+    plt.legend()
+    plt.show()
+    print("Best individual is:", hof[0], "with fitness:", hof[0].fitness)
