82e407efef01cfae972db1928638696b31ce92f7
Katie Greer <katieggreer@gmail.com>
2024-05-17 21:22:02 -0600

    altered grid to have specified height and width instead of square dimensions, altered agent to spawn in random point of grid, removed obstacles

--- constants.py
+++ constants.py
@@ -8,6 +8,16 @@ RULES = {
         "<op>" :["left","right","move"] 
     }
 
+
+# <prog> ::= <code>
+# <code> ::= <code> | <progs>
+# <progs>::= <condition> | <prog2> | <prog3> | <op>
+# <condition>::=if_food_ahead(<progs>,<progs>)
+# <prog2>::=prog2(<progs>,<progs>)
+# <prog3>::=prog3(<progs>,<progs>,<progs>)
+# <op>::=left|right|move
+
+
 # evolve constants
 GENERATIONS = 100
 
@@ -19,5 +29,29 @@ WEST = 3
 NUM_AGENTS = 50
 
 # grid constants
-GRID_SIZE = 32
+GRID_WIDTH = 32
+GRID_HEIGHT = 32
 
+
+# TODO: figure out which constants are relevant
+# CACHE:                  True
+# CODON_SIZE:             100
+# CROSSOVER:              variable_onepoint
+# CROSSOVER_PROBABILITY:  0.9
+# DEBUG:                  False
+# GENERATIONS:	        1
+# MAX_GENOME_LENGTH:      500
+# GRAMMAR_FILE:           ..,santa_fe_trail.bnf
+# INITIALISATION:         PI_grow
+# INVALID_SELECTION:      True
+# MAX_INIT_TREE_DEPTH:    12
+# MAX_TREE_DEPTH:         16
+# MUTATION:               int_flip_per_codon
+# POPULATION_SIZE:	    100
+# FITNESS_FUNCTION:       santa_fe_trail.santa_fe_trail
+# REPLACEMENT:            generational
+# SELECTION:              truncation
+# SELECTION_PROPORTION:   0.3
+# TOURNAMENT_SIZE:        5
+# MUTATION_PROBABILITY:   0.01
+# VERBOSE:                False
