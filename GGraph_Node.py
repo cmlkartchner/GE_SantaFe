064ee3a8552f214dd7bdb7b1bb5f59fa1db44238
rhyusarmiento <rhyu.sar.15@gmail.com>
2024-06-18 12:00:11 -0600

    work on Graph

--- GGraph_Node.py
+++ GGraph_Node.py
@@ -4,12 +4,39 @@ class GGraph:
     # create nodes based of dict
     def __init__(self) -> None:
         self.Nodes = []
+        
+    # return True if node is in graph
+    def isNode(self, rule):
+        for node in self.Nodes:
+            if rule == node.get_value():
+                return True
+        return False
+    
+    def getNode(self, rule):
+        for node in self.Nodes:
+            if rule == node.get_value():
+                return node
+            
+    # generates Graph
     def generateGraph(self, rules):
-        # create core node
-        # loop or recursive? recur; doesn't recur on existing nodes and terminal
-        # at every option, need to find all non termials that for option and make nodes
         # check if node exist in Graph
-        # create node for each non existant relative node and set in node list
+        for rule in rules:
+            currNode = None
+            if not self.isNode(rule):
+                currNode = Node(rule)
+                self.Nodes.append(currNode)
+            else:
+                currNode = self.getNode(rule)
+            # create node for each non existant relative node and set in node list
+            for relative in rules[rule]:
+                relNode = None
+                if not self.isNode(relative):
+                    relNode = Node(relative)
+                    self.Nodes.append(relNode)
+                else:
+                    relNode = self.getNode(relative)
+                currNode.appendNode(relNode)
+                
         # for every relative terminal node; increase weight
         # for every relative with weight decrease score
         # NOTE: right hand equivanance bias selection
@@ -24,15 +51,20 @@ class GGraph:
         pass
 
 class Node:
-    def __init__(self) -> None:
+    def __init__(self, inValue) -> None:
         # termial in this context means that there is no <> non terminals in the statement
         self.Nodes = []
-        self.value = ''
-        self.isTerminal = False
+        self.value = inValue
+        if "<" not in inValue and ">" not in inValue:
+            self.isTerminal = True
+        else: 
+            self.isTerminal = False
         self.weight = 0
+    def appendNode(self, node):
+        self.Nodes.append(node)
+    def get_value(self):
+        return self.value
     def find_mod_node():
         pass
     def find_weight_node():
         pass
-    
-    
