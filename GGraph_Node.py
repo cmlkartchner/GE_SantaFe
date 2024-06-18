5cc8f50788b56e88b63bb32873ae86dc5b4dfdbb
rhyusarmiento <rhyu.sar.15@gmail.com>
2024-06-18 00:22:13 -0600

    Attempt with grammer graph

---
+++ GGraph_Node.py
@@ -0,0 +1,30 @@
+class GGraph:
+    # Todo: 
+    # search by non terminal and return a node/value relative to the mod
+    # create nodes based of dict
+    def __init__(self) -> None:
+        self.Nodes = []
+    def generateGraph(self, rules):
+        # create core node
+        # check if node exist in Graph
+        # create node for each non existant relative node and set
+        # for every relative terminal node; increase weight
+        pass
+    def find_by_mod():
+        pass
+    def find_by_weight():
+        pass
+
+class Node:
+    def __init__(self) -> None:
+        # termial in this context means that there is no <> non terminals in the statement
+        self.Nodes = []
+        self.value = ''
+        self.isTerminal = False
+        self.weight = 0
+    def find_mod_node():
+        pass
+    def find_weight_node():
+        pass
+    
+    
