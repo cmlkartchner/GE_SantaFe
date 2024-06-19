0f460078ee3c08863497e422a499af57ba5d2722
rhyusarmiento <rhyu.sar.15@gmail.com>
2024-06-19 17:43:16 -0600

    generateGraph done

--- GGraph_Node.py
+++ GGraph_Node.py
@@ -17,54 +17,58 @@ class GGraph:
             if rule == node.get_value():
                 return node
             
+    # if node exist return if not create new
+    def selectNode(self, header):
+        if not self.isNode(header):
+            currNode = Node(header)
+            self.Nodes.append(currNode)
+        else:
+            currNode = self.getNode(header)
+        return currNode
+    
+    # depth first traversal 
+    def updateWeights(self, coreNode):
+        for node in coreNode.inNodes:
+            if node.weight < (coreNode.weight - 1):
+                node.weight = coreNode.weight - 1
+                self.updateWeights(node)
+    
     # generates Graph
     def generateGraph(self, rules):
         # check if node exist in Graph
         for rule in rules:
-            currNode = None
-            if not self.isNode(rule):
-                currNode = Node(rule)
-                self.Nodes.append(currNode)
-            else:
-                currNode = self.getNode(rule)
-            # create node for each non existant relative node and set in node list
-            for relative in rules[rule]:
-                relNode = None
-                if not self.isNode(relative):
-                    relNode = Node(relative)
-                    self.Nodes.append(relNode)
-                else:
-                    relNode = self.getNode(relative)
-                currNode.appendNode(relNode)
-                
-        # for every relative terminal node; increase weight
-        # for every relative with weight decrease score
-        # NOTE: right hand equivanance bias selection
-        # NOTE: use geneotype for new termial selection? Node termaial non terminal lists?
-        # TODO: think about weight calcuation; during/after?
-        # NOTE: no repeative relative nodes
-        # NOTE: update parents? because of recur
-        pass
+            trunckNode = self.selectNode(rule)
+            # create node for each non existant production node and set in node list
+            terminalCount = 0
+            for production in rules[rule]:
+                branchNode = self.selectNode(production)
+                if branchNode.isTerminal is True:
+                    terminalCount += 1
+                # set truck weight only if its more
+                if trunckNode.weight < terminalCount:
+                    trunckNode.weight = terminalCount    
+                trunckNode.appendOutNode(branchNode)
+                branchNode.appendInNode(trunckNode)
+                self.updateWeights(branchNode)
     def find_by_mod():
         pass
     def find_by_weight():
         pass
+                
+        # NOTE: use geneotype for new termial selection? Node termaial non terminal lists?
 
 class Node:
     def __init__(self, inValue) -> None:
         # termial in this context means that there is no <> non terminals in the statement
-        self.Nodes = []
+        self.inNodes = []
+        self.outNodes = []
         self.value = inValue
         if "<" not in inValue and ">" not in inValue:
             self.isTerminal = True
         else: 
             self.isTerminal = False
         self.weight = 0
-    def appendNode(self, node):
-        self.Nodes.append(node)
-    def get_value(self):
-        return self.value
-    def find_mod_node():
-        pass
-    def find_weight_node():
-        pass
+    def appendOutNode(self, node):
+        self.outNodes.append(node)
+    def appendInNode(self, node):
+        self.outNodes.append(node)
